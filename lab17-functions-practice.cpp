2d41767968d15641e7c6cc4823c88b79ec8a7711
bernhalx <bernhalx@udmercy.edu>
2025-10-22 16:01:15 -0400

    Update lab17-functions-practice.cpp

--- lab17-functions-practice.cpp
+++ lab17-functions-practice.cpp
@@ -2,7 +2,7 @@
 // Gets three exam scores from the user and shows them 
 // their letter grades
 // Started by: Caitlin Snyder
-// Edited by: [NAME HERE]
+// Edited by: [Liam Bernhard and Mouhameth Ba]
 
 #include <iostream>
 using namespace std;
@@ -19,7 +19,22 @@ using namespace std;
     // Ask the user for a score and validate input.
     // Keep asking until the score is between 0 and 100.
     // Return the valid score.
+// The function getScore's purpose is to recive a test score fro mthe user and validate in and store in into a variabe in the main funtion to use later.
+// There is no input parameter in this function but it will output the validated test score.
+double getScore() {
+    // We initalized score0 so the user could enter a test score number.
+    double score0;
+    // We used a do while to validate that the user enters a valid test score between (0 and 100)
+    do {
+        cout << "Enter a test score with a grade ranging from (0 - 100) here: ";
+        cin >> score0;
+        
 
+            
+    } while (score0 < 0 || score0 > 100);
+    // This line returns the validated score to the getscore() in the main function.
+    return score0;
+}
 
 
 
@@ -32,6 +47,7 @@ using namespace std;
  * Return Value: None (displays the letter grade to the screen)
  ****************************************************************/
 
+
 // [TO DO : ADD getScore function HERE]
     // Determine the letter grade and display it.
     // Example grading scale:
@@ -40,6 +56,27 @@ using namespace std;
     // 70–79:  C
     // 60–69:  D
     // Below 60: F
+// This is the void function called dipslayLetterGrade that will display the letter grade based on the score entered.
+// The inputed parameter of the function is the score entered previously. There is nothing returned in this function.
+// This is a series of if, else if, and else statements were used to assign a letter to its appropriate score.
+void displayLetterGrade(double score) {
+    if (score >= 90) {
+        cout << "You have A " << endl;
+    }
+    else if (score >= 80 && score <= 89) {
+    cout << "You have B " << endl;
+}
+    else if (score >= 70 && score <= 79) {
+        cout << "You have C " << endl;
+    }
+    else if (score >= 60 && score <= 69) {
+        cout << "You have D " << endl;
+    }
+     else {
+         cout << "You are a failure" << endl;
+     }
+ }
+
 
 
 
